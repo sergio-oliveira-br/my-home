f43757ce301f69d81b870a57a0414633b7c67ed3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-12 14:24:30 +0100

    Create PersonService.java
    act:
    -> service class created - methods CRUD created

---
+++ src/main/java/com/github/sergiooliveirabr/myhome/service/PersonService.java
@@ -0,0 +1,43 @@
+package com.github.sergiooliveirabr.myhome.service;
+
+import com.github.sergiooliveirabr.myhome.entity.Person;
+import com.github.sergiooliveirabr.myhome.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public void save(Person person) {
+        personRepository.save(person);
+    }
+
+    public Person findById(Long id) {
+        Optional<Person> person = personRepository.findById(id);
+        if (person.isPresent()) {
+            return person.get();
+        }
+        throw new NoSuchElementException("Person with ID " + id + " not found");
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        personRepository.deleteById(id);
+    }
+}
