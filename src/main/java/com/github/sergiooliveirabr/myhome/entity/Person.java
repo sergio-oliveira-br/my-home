a88a3d3daa8cfff6f709f6aade2d1af1eb5a01d3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-13 13:06:41 +0100

    Update Person.java
    Issue:
    The lombok is not working with thymeleaf
    
    act
    -> setter and getters created by me

--- src/main/java/com/github/sergiooliveirabr/myhome/entity/Person.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/entity/Person.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Data
+//@Data
 public class Person {
 
     @Id
@@ -15,4 +15,28 @@ public class Person {
 
     @Enumerated(EnumType.STRING)
     private Room assignedRoom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Room getAssignedRoom() {
+        return assignedRoom;
+    }
+
+    public void setAssignedRoom(Room assignedRoom) {
+        this.assignedRoom = assignedRoom;
+    }
 }
