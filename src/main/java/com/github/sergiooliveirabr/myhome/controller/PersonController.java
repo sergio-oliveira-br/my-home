8e8c26ed7c9fed9cd226f6cc56d1e0290ef2e6f2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-13 13:09:55 +0100

    Update PersonController.java
    'Cause I'm using MVC approach, I should not use the ResponseEntity
    
    Act
    -> set the method to return a view
    -> method GET created to adjusted to MVC
    -> Model attribute added to capture the info from form

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/PersonController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/PersonController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,9 +23,16 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @GetMapping
+    public String viewPersonPage(Model model) {
+        model.addAttribute("person", new Person());
+        model.addAttribute("people", personService.findAll());
+        return "person";
+    }
+
     @PostMapping("/add")
-    public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+    public String addPerson(@ModelAttribute Person person) {
         personService.save(person);
-        return ResponseEntity.status(HttpStatus.CREATED).body(person);
+        return "redirect:/person";
     }
 }
