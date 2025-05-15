bbee5bf0f182b592f8602cba242b9c991a72e1b5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-15 20:54:21 +0100

    Update CleaningSchedule.java
    act:
    getters and setters created

--- src/main/java/com/github/sergiooliveirabr/myhome/entity/schedule/CleaningSchedule.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/entity/schedule/CleaningSchedule.java
@@ -1,7 +1,6 @@
 package com.github.sergiooliveirabr.myhome.entity.schedule;
 
 import com.github.sergiooliveirabr.myhome.entity.cleaning.Room;
-import com.github.sergiooliveirabr.myhome.entity.person.Person;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -17,4 +16,28 @@ public class CleaningSchedule {
 
     @Enumerated(EnumType.STRING)
     private Room room;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCleaningDate() {
+        return cleaningDate;
+    }
+
+    public void setCleaningDate(LocalDateTime cleaningDate) {
+        this.cleaningDate = cleaningDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
