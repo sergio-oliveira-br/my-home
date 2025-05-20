f865af6ff1dd44cd71a8da025ec86ba43edc9d18
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-20 12:04:23 +0100

    Update StartScheduleController.java
    act:
    -> model created
    this will create a list by using the ENUM "Room"

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/StartScheduleController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/StartScheduleController.java
@@ -1,6 +1,8 @@
 package com.github.sergiooliveirabr.myhome.controller;
 
+import com.github.sergiooliveirabr.myhome.entity.cleaning.Room;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StartScheduleController {
 
     @GetMapping
-    public String viewStartSchedule() {
+    public String viewStartSchedule(Model model) {
+
+        //This will display the options of the rooms avaible to start the schedule
+        model.addAttribute("roomoptions", Room.values());
+
         return "config/start-schedule";
     }
+
 }
