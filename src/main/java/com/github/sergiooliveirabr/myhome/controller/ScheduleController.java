72fecd61f07e0e741f8dac68496068f83f56b7e5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-21 17:59:41 +0100

    Update ScheduleController.java
    act:
    -> display the schedule as model on the schedule page implemented

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/ScheduleController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/ScheduleController.java
@@ -1,15 +1,28 @@
 package com.github.sergiooliveirabr.myhome.controller;
 
+import com.github.sergiooliveirabr.myhome.entity.cleaning.Room;
+import com.github.sergiooliveirabr.myhome.service.schedule.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+    private final ScheduleService scheduleService;
+
+    @Autowired
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
     @GetMapping
-    public String viewSchedule() {
+    public String viewSchedulePage(Model model) {
+        model.addAttribute("schedules", scheduleService.getCurrentSchedule());
+        //This will display the options of the rooms avaible to start the schedule
+        model.addAttribute("roomoptions", Room.values());
         return "schedule";
     }
 }
