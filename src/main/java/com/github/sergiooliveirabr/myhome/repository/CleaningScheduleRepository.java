84c2caba39d4353e256e0f8e7b6f6f5d2e123137
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-21 17:52:15 +0100

    Update CleaningScheduleRepository.java
    act:
    -> Query Method included within repository

--- src/main/java/com/github/sergiooliveirabr/myhome/repository/CleaningScheduleRepository.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/repository/CleaningScheduleRepository.java
@@ -2,6 +2,12 @@ package com.github.sergiooliveirabr.myhome.repository;
 
 import com.github.sergiooliveirabr.myhome.entity.schedule.CleaningSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CleaningScheduleRepository extends JpaRepository<CleaningSchedule, Long> {
+
+    List<CleaningSchedule> findByCompletedFalse();
 }
