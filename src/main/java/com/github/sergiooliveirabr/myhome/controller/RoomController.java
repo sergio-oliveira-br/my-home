0f193e8073089fcbc4c3960c4c51df0cc290fcd1
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-22 11:25:49 +0100

    Create RoomController.java
    Act:
    -> controlador created.
    Enables the creation and display of rooms on the room page

---
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/RoomController.java
@@ -0,0 +1,33 @@
+package com.github.sergiooliveirabr.myhome.controller;
+
+import com.github.sergiooliveirabr.myhome.entity.Room;
+import com.github.sergiooliveirabr.myhome.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/room")
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @Autowired
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public String room(Model model) {
+        model.addAttribute("room", new Room());
+        model.addAttribute("allrooms", roomService.findAll());
+        return "config/room";
+    }
+
+    @PostMapping("/add")
+    public String addRoom(@ModelAttribute Room room) {
+        roomService.save(room);
+        return "redirect:/room";
+    }
+}
