71fce5e20ca382f3277cfe128cd8b0d2285b41fe
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-23 19:34:55 +0100

    Update Cleaning.java
    act:
    -> attribute responsibleRoom created
    -> getters and setters created

--- src/main/java/com/github/sergiooliveirabr/myhome/entity/Cleaning.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/entity/Cleaning.java
@@ -12,7 +12,44 @@ public class Cleaning {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "responsible_person_id")
     private Person responsiblePerson;
 
+    @ManyToOne
+    @JoinColumn(name = "responsible_room_id")
+    private Room responsibleRoom;
+
     private LocalDate dataHeld;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getResponsiblePerson() {
+        return responsiblePerson;
+    }
+
+    public void setResponsiblePerson(Person responsiblePerson) {
+        this.responsiblePerson = responsiblePerson;
+    }
+
+    public Room getResponsibleRoom() {
+        return responsibleRoom;
+    }
+
+    public void setResponsibleRoom(Room responsibleRoom) {
+        this.responsibleRoom = responsibleRoom;
+    }
+
+    public LocalDate getDataHeld() {
+        return dataHeld;
+    }
+
+    public void setDataHeld(LocalDate dataHeld) {
+        this.dataHeld = dataHeld;
+    }
 }
