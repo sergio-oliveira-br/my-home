e73c81a1977f7db70a0a8ffa8998a9e7009debf9
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 13:48:43 +0100

    Update PastCleaningService.java
    act:
    -> method findLastCleaning added from repository

--- src/main/java/com/github/sergiooliveirabr/myhome/service/PastCleaningService.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/service/PastCleaningService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PastCleaningService {
@@ -19,5 +20,9 @@ public class PastCleaningService {
 
     public List<Cleaning> findAll() {
         return cleaningRepository.findAll();
+    }
+
+    public Optional<Cleaning> findLastCleaning() {
+        return cleaningRepository.findTopByOrderByIdDesc();
     }
 }
