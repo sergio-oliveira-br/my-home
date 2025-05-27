edc4fda28ad25f76e8d604eaa3b29c5d66b77dd2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 13:49:41 +0100

    Update IndexController.java
    act:
    -> attribute "last_cleaning"  added to display the info in within card on index page

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/IndexController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.github.sergiooliveirabr.myhome.controller;
 
+import com.github.sergiooliveirabr.myhome.service.PastCleaningService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/index")
 public class IndexController {
 
+    private final PastCleaningService pastCleaningService;
+
+    @Autowired
+    public IndexController(PastCleaningService pastCleaningService) {
+        this.pastCleaningService = pastCleaningService;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        String date = String.valueOf(pastCleaningService.findLastCleaning().get().getDataHeld());
+        String person = pastCleaningService.findLastCleaning().get().getResponsiblePerson().getName();
+
+        model.addAttribute("last_cleaning",
+                person + " carried out the most recent cleaning on " + date);
         return "index";
     }
 }
