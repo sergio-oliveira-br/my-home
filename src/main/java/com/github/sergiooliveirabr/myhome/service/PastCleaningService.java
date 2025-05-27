92eb0f760d53b763a00160001e84d719846e8708
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 14:10:10 +0100

    Update PastCleaningService.java
    This method will be responsible to display how many days the last cleaning has been done
    
    Act:
    -> getFormattedLastCleaningDetails created

--- src/main/java/com/github/sergiooliveirabr/myhome/service/PastCleaningService.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/service/PastCleaningService.java
@@ -5,6 +5,8 @@ import com.github.sergiooliveirabr.myhome.repository.CleaningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +26,30 @@ public class PastCleaningService {
 
     public Optional<Cleaning> findLastCleaning() {
         return cleaningRepository.findTopByOrderByIdDesc();
+    }
+
+    public String getFormattedLastCleaningDetails() {
+        Optional<Cleaning> lastCleaningOptional = findLastCleaning();
+
+        String dateDisplay = null;
+
+        if (lastCleaningOptional.isPresent()) {
+            Cleaning lastCleaning = lastCleaningOptional.get();
+
+            LocalDate lastCleaningDate = lastCleaning.getDataHeld();
+            LocalDate today = LocalDate.now();
+
+            long daysDifference = ChronoUnit.DAYS.between(lastCleaningDate, today);
+            String lastDone = "Last Done | ";
+
+            if (daysDifference == 0) {
+                dateDisplay = lastDone +  "Today";
+            } else if (daysDifference == 1) {
+                dateDisplay = lastDone + "Yesterday";
+            } else if (daysDifference > 1) {
+                dateDisplay = lastDone + daysDifference + " days ago";
+            }
+        }
+        return dateDisplay;
     }
 }
