49aca66cbfb2ea3e78d2acc13af0079180d4d799
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 12:50:24 +0100

    Create PastCleaningService.java
    Act:
    -> Method find all created

---
+++ src/main/java/com/github/sergiooliveirabr/myhome/service/PastCleaningService.java
@@ -0,0 +1,23 @@
+package com.github.sergiooliveirabr.myhome.service;
+
+import com.github.sergiooliveirabr.myhome.entity.Cleaning;
+import com.github.sergiooliveirabr.myhome.repository.CleaningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PastCleaningService {
+
+    private final CleaningRepository cleaningRepository;
+
+    @Autowired
+    public PastCleaningService(CleaningRepository cleaningRepository) {
+        this.cleaningRepository = cleaningRepository;
+    }
+
+    public List<Cleaning> findAll() {
+        return cleaningRepository.findAll();
+    }
+}
