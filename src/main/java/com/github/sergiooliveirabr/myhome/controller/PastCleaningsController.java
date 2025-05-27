83063324bf5bf5d69b1c96c8629947f56532bd78
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 12:53:42 +0100

    Update PastCleaningsController.java
    act:
    -> attribute added to make it possible to see the history in table

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/PastCleaningsController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/PastCleaningsController.java
@@ -1,6 +1,10 @@
 package com.github.sergiooliveirabr.myhome.controller;
 
+import com.github.sergiooliveirabr.myhome.entity.Cleaning;
+import com.github.sergiooliveirabr.myhome.service.PastCleaningService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/past-cleanings")
 public class PastCleaningsController {
 
+    private final PastCleaningService pastCleaningService;
+
+    @Autowired
+    public PastCleaningsController(PastCleaningService pastCleaningService) {
+        this.pastCleaningService = pastCleaningService;
+    }
+
     @GetMapping
-    public String viewPastCleanings() {
+    public String viewPastCleanings(Model model) {
+        model.addAttribute("history", pastCleaningService.findAll());
         return "past-cleanings";
     }
 }
