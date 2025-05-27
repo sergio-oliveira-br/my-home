5a0a4a52a65eaf4d35cbe37b4ee225fd4a995ea2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-27 13:48:10 +0100

    Update CleaningRepository.java
    act:
    -> findTopByOrderByIdDesc added. This method is provided by Spring Data JPA's query derivation mechanism

--- src/main/java/com/github/sergiooliveirabr/myhome/repository/CleaningRepository.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/repository/CleaningRepository.java
@@ -4,6 +4,9 @@ import com.github.sergiooliveirabr.myhome.entity.Cleaning;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CleaningRepository extends JpaRepository<Cleaning, Long> {
+    Optional<Cleaning> findTopByOrderByIdDesc();
 }
