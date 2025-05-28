de8d211679be3498a2aea412a41b43e08f1431ca
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-28 13:35:46 +0100

    Update RoomService.java
    Act:
    -> getNextRoomInRotation() created.
    This method will get the next room based on the last room. It is also make a looping rotation

--- src/main/java/com/github/sergiooliveirabr/myhome/service/RoomService.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/service/RoomService.java
@@ -1,20 +1,25 @@
 package com.github.sergiooliveirabr.myhome.service;
 
+import com.github.sergiooliveirabr.myhome.entity.Cleaning;
 import com.github.sergiooliveirabr.myhome.entity.Room;
+import com.github.sergiooliveirabr.myhome.repository.CleaningRepository;
 import com.github.sergiooliveirabr.myhome.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final CleaningRepository cleaningRepository;
 
     @Autowired
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, CleaningRepository cleaningRepository) {
         this.roomRepository = roomRepository;
+        this.cleaningRepository = cleaningRepository;
     }
 
     public Room save(Room room) {
@@ -23,5 +28,28 @@ public class RoomService {
 
     public List<Room> findAll() {
         return roomRepository.findAll();
+    }
+
+    public Room getNextRoomInRotation() {
+
+        // Find the rooms
+        List<Room> allRooms = roomRepository.findAllByOrderByIdAsc();
+        if (allRooms.isEmpty()) {
+           throw new IllegalArgumentException("There is no next room");
+        }
+
+        // Find the last room that has done the cleaning
+        Optional<Cleaning> lastCleaning = cleaningRepository.findTopByOrderByIdDesc();
+        if (lastCleaning.isEmpty()) {
+            return allRooms.getFirst();
+        }
+
+        Room lastRoom = lastCleaning.get().getResponsiblePerson().getRoom();
+
+        int indexRoom = allRooms.indexOf(lastRoom);
+
+        int nextIndex = (indexRoom + 1) % allRooms.size();
+
+        return allRooms.get(nextIndex);
     }
 }
