6529029b3b7b97c67d884f2030a9b41d0d5002cc
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-28 13:34:26 +0100

    Update RoomRepository.java
    Act:
    ->  List<Room> findAllByOrderByIdAsc() implemented by JPA Repository

--- src/main/java/com/github/sergiooliveirabr/myhome/repository/RoomRepository.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/repository/RoomRepository.java
@@ -4,6 +4,10 @@ import com.github.sergiooliveirabr.myhome.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findAllByOrderByIdAsc();
 }
