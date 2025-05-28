be8698d554669c130414ab90ede1d7cac9069c08
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-28 13:36:26 +0100

    Update IndexController.java
    act:
    -> model implemented on the first card on index page. This will display the next room based on last room (ref.id)

--- src/main/java/com/github/sergiooliveirabr/myhome/controller/IndexController.java
+++ src/main/java/com/github/sergiooliveirabr/myhome/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.github.sergiooliveirabr.myhome.controller;
 
 import com.github.sergiooliveirabr.myhome.service.PastCleaningService;
+import com.github.sergiooliveirabr.myhome.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     private final PastCleaningService pastCleaningService;
+    private final RoomService roomService;
 
     @Autowired
-    public IndexController(PastCleaningService pastCleaningService) {
+    public IndexController(PastCleaningService pastCleaningService, RoomService roomService) {
         this.pastCleaningService = pastCleaningService;
+        this.roomService = roomService;
     }
 
     @GetMapping
@@ -24,7 +27,13 @@ public class IndexController {
         String date = String.valueOf(pastCleaningService.findLastCleaning().get().getDataHeld());
         String person = pastCleaningService.findLastCleaning().get().getResponsiblePerson().getName();
 
-        model.addAttribute("display_days_ago", pastCleaningService.getFormattedLastCleaningDetails());
+        //First card (Next Schedule)
+        model.addAttribute("next_room",
+                "The next cleaning is up to " + roomService.getNextRoomInRotation().getName());
+
+        //Second card (Past Cleaning)
+        model.addAttribute("display_days_ago",
+                pastCleaningService.getFormattedLastCleaningDetails());
 
         model.addAttribute("last_cleaning",
                 person + " carried out the most recent cleaning on " + date);
